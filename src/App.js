f02d3b078d8a9ec2ba992da7b36850bcafbf53e8
Amirhosein <amirmoghaddasi11@gmail.com>
2021-05-22 13:18:31 +0430

    add search and checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from "react";
 import List from "./components/List";
 import Movies from "./data.json";
-function App() {
-  const data = Movies.map((item) => ({
-    id: item.id,
-    text: item.name,
-    rate: item.rate,
-  }));
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeywordChange = this.handleKeywordChange.bind(this);
+    this.handleOverSevenChange = this.handleOverSevenChange.bind(this);
+    this.state = {
+      keyword: "",
+      overSeven: false,
+    };
+  }
+  handleKeywordChange(e) {
+    this.setState({ keyword: e.target.value });
+  }
+  handleOverSevenChange(e) {
+    this.setState({ overSeven: e.target.checked });
+  }
+  render() {
+    const { keyword, overSeven } = this.state;
+    const data = Movies.filter((item) =>
+      item.name.toLowerCase().includes(keyword.toLowerCase())
+    )
+      .filter((item) => (overSeven ? item.rate > 7 : true))
+      .map((item) => ({
+        id: item.id,
+        text: item.name,
+        rate: item.rate,
+      }));
+    return (
       <div>
-        keyword
-        <input type="text" />
         <div>
-          over 7
-          <input type="checkbox" />
+          keyword
+          <input
+            type="text"
+            value={keyword}
+            onChange={this.handleKeywordChange}
+          />
+          <div>
+            over 7
+            <input
+              type="checkbox"
+              checked={overSeven}
+              onChange={this.handleOverSevenChange}
+            />
+          </div>
         </div>
+        <List items={data} />
       </div>
-      <List items={data} />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
